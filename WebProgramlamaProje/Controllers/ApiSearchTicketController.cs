de1c9d93df756399048d21ff6c43b96e99b0347d
utkugzl <utku.guzel44@gmail.com>
2023-12-16 16:25:09 +0300

    ticket search api entegration

---
+++ WebProgramlamaProje/Controllers/ApiSearchTicketController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebProgramlamaProje.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebProgramlamaProje.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiSearchTicketController : ControllerBase
+    {
+        private Context context = new Context();
+
+        // GET: api/<ApiSearchTicketController>
+        //[HttpGet]
+        //public IEnumerable<FlightBooking> Get()
+        //{
+        //    var ticket = context.FlightBookings.ToList();
+
+        //    return ticket;
+        //}
+
+        // GET api/<ApiSearchTicketController>/5
+        [HttpGet("{ticketNo}")]
+        public ActionResult<FlightBooking> Get(int? ticketNo)
+        {
+            if (ticketNo is null)
+            {
+                return NotFound();
+            }
+            var ticket = context.FlightBookings.FirstOrDefault(x => x.TicketNumber == ticketNo);
+
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            return ticket;
+        }
+
+        // POST api/<ApiSearchTicketController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ApiSearchTicketController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ApiSearchTicketController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- WebProgramlamaProje/Controllers/HomeController.cs
+++ WebProgramlamaProje/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using WebProgramlamaProje.Models;
 
 namespace WebProgramlamaProje.Controllers
@@ -27,5 +28,69 @@ namespace WebProgramlamaProje.Controllers
             ViewBag.searchedFlights = control;
             return View();
         }
+
+
+        public async Task<IActionResult> SearchTicket(int ticketNo)
+        {
+            FlightBooking ticket = null;
+
+            using (HttpClient client = new HttpClient())
+            {
+                // Ticket numarasını içeren URL'yi oluştur
+                string apiUrl = $"https://localhost:7163/api/ApiSearchTicket/{ticketNo}";
+
+                // GET isteği yap
+                var response = await client.GetAsync(apiUrl);
+
+                // Yanıtı oku
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseText = await response.Content.ReadAsStringAsync();
+
+                    try
+                    {
+                        // JSON'u deserialize et
+                        ticket = JsonConvert.DeserializeObject<FlightBooking>(responseText);
+                    }
+                    catch (JsonSerializationException)
+                    {
+                        // JSON bir dizi değilse veya deserialization hatası oluşursa
+                        // Tek bir nesne olarak deserialize etmeye çalış
+                        ticket = JsonConvert.DeserializeObject<FlightBooking>(responseText);
+                    }
+                }
+                else
+                {
+                    // Başarısız bir durumda NotFound döndür
+                    return NotFound();
+                }
+            }
+
+            var flight = context.Flights.FirstOrDefault(f => f.FlightID == ticket.FlightId);
+            ticket.Flight = flight;
+            // ticket null değilse View'e gönder
+            if (ticket != null)
+            {
+
+                return View(ticket);
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Belirtilen bilet numarası bulunamadı.";
+                return RedirectToAction("Error");
+            }
+
+
+
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+
+
+
     }
 }
